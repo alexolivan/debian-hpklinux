8bed0f276e76e0a04a2f6cb4664b79560e79b0e5
Fred Gleason <fredg@paravelsystems.com>
2024-07-19 15:48:15 -0400

    2024-07-19 Fred Gleason <fredg@paravelsystems.com> 	* Forked to 'hpi4.20.44'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- versions.sh
+++ versions.sh
@@ -4,6 +4,6 @@
 # When incrementing versions, be sure to create a new entry in
 # 'debian/changelog'!
 #
-ASIHPI_PKG_VERSION=4.20.43-1
-ASIHPI_VERSION=4.20.43
+ASIHPI_PKG_VERSION=4.20.44-1
+ASIHPI_VERSION=4.20.44
 ASIHPI_MAJOR_VERSION=4.20
