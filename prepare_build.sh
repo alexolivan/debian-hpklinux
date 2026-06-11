1f27abdc4365943ca315223394d2cdcc66da640d
Alejandro Olivan <alejandro.olivan.alvarez@gmail.com>
2026-06-11 18:49:35 +0200

    feat(dkms): native dual support for HPI and ALSA
    - Removed snd-asihpi blacklist in rules.src
    - Configured dual and independent extraction of hpi-drv and alsa-drv
    - On-the-fly sanitization of the original AudioScience dkms.conf
    - Bimodal registration, build, and installation in postinst.src
    - Ensured thorough cleanup of both modules in prerm.src
    - Cleaned prepare_build.sh of residual hacks
    - Updated changelog to reflect authorship and v4.20.54 changes

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,11 +2,16 @@
 
 . ./versions.sh
 
+# 1. Limpieza de residuos
 rm -f *.buildinfo *.changes *.ddeb *.deb *.dsc *.debian.tar.xz
 rm -rf hpklinux_$ASIHPI_VERSION
 rm -rf hpklinux_$ASIHPI_VERSION-orig
 rm -f debian/control
+
+# 2. Extraemos el código fuente original de AudioScience
 tar -zvxf hpklinux_$ASIHPI_VERSION.tar.gz
+
+# 3. Procesamos las plantillas de control de Fred como siempre
 sed s/@ASIHPI_PKG_VERSION@/$ASIHPI_PKG_VERSION/ < debian/control.src > debian/control
 sed s/@ASIHPI_VERSION@/$ASIHPI_VERSION/ < debian/rules.src > debian/rules
 chmod 755 debian/rules
@@ -18,9 +23,14 @@ chmod 755 debian/prerm
 rm debian/prerm.src2
 sed s/@ASIHPI_VERSION@/$ASIHPI_VERSION/ < debian/postinst.src > debian/postinst.src2
 sed s/@ASIHPI_MAJOR_VERSION@/$ASIHPI_MAJOR_VERSION/ < debian/postinst.src2 > debian/postinst
-sed -i "1s/hpklinux (.*)/hpklinux ($ASIHPI_PKG_VERSION)/" debian/changelog
 chmod 755 debian/postinst
 rm debian/postinst.src2
+
+# 4. Tu automatización del changelog
 sed -i "1s/hpklinux (.*)/hpklinux ($ASIHPI_PKG_VERSION)/" debian/changelog
+
+# 5. Metemos la carpeta debian dentro del código fuente modificado
 cp -a debian hpklinux_$ASIHPI_VERSION/
+
+# 6. Re-empaquetamos el tarball .orig que usará dpkg-buildpackage para construir el .deb
 tar -zvcf hpklinux_$ASIHPI_VERSION.orig.tar.gz hpklinux_$ASIHPI_VERSION
